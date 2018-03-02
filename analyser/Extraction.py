b9b85db0935559d683347a4abb9ca3ad6820ecb5
kushalkolar <kushalkolar@gmail.com>
2018-03-02 21:36:44 +0100

    Update Extraction.py

---
+++ analyser/Extraction.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri March 2 2017
+
+@author: kushal
+
+Chatzigeorgiou Group
+Sars International Centre for Marine Molecular Biology
+
+GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+"""
+
+
+class PeakFeatures:
+    def __init__(self, transmission):
+        pass
+
+    def amplitude(self):
+        pass
+
+    def area(self):
+        pass
+
+    def rising_slope(self):
+        pass
+
+    def falling_slop(self):
+        pass
+
+    def duration_base(self):
+        pass
+
+    def duration_mid(self):
+        pass
+
+    def inter_peak_interval(self):
+        pass
