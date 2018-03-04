f31f2941f213290020a2795c9407000ab0bb07bf
kushalkolar <kushalkolar@gmail.com>
2018-03-04 01:42:27 +0100

    Update ComputeInterfaces.py and Extraction.py

--- analyser/ComputeInterfaces.py
+++ analyser/ComputeInterfaces.py
@@ -14,28 +14,21 @@ GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 from multiprocessing import Process, Queue
 
 
-class Interface:
+class Static:
     def __init__(self, compute_class, args_dict):
-        self.funcs = [f for f in compute_class.__dict__.items() if callable(f)]
+        self.compute_class = compute_class
         self.args_dict = args_dict
+        self.func_names = [f for f in self.args_dict.keys() if hasattr(self.compute_class, f)]
         self.Q = Queue()
 
     def run(self):
-        for func in self.funcs:
-            if func[0] not in self.args_dict.keys():
-                continue
-
-            a = tuple([self.Q] + self.args_dict[func[0]])
-            proc = Process(target=func[1], args=a)
+        for func in self.func_names:
+            f = getattr(self.compute_class, func)
+            a = tuple([self.Q] + self.args_dict[func])
+            proc = Process(target=f, args=a)
             proc.start()
             proc.join()
 
     def get_results(self):
         return self.Q.get()
-
-
-class Static(Interface):
-    def __init__(self, compute_class, args_dict):
-        Interface.__init__(self, compute_class, args_dict)
-        self.funcs = [f for f in compute_class.__dict__.items() if type(f[1]) == staticmethod]
 
