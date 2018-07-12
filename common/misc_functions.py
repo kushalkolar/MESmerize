10f303aaafd4dc427b0cc80869a2340dd5cf0bff
kushalkolar <kushalkolar@gmail.com>
2018-07-13 01:04:40 +0200

    Update misc_functions.py and mesfile.py

--- common/misc_functions.py
+++ common/misc_functions.py
@@ -11,7 +11,33 @@ Sars International Centre for Marine Molecular Biology
 GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 
 """
+import scipy.io as spio
 
 
-def floating_point_equality(m: float, n: float, epsilon: float = 0.0001) -> bool:
+def are_floats_equal(m: float, n: float, epsilon: float = 0.0001) -> bool:
     return abs((m + 0.0) - (n + 0.0)) < epsilon
+
+
+class MatlabFuncs:
+    @staticmethod
+    def loadmat(filename: str) -> dict:
+        data = spio.loadmat(filename, struct_as_record=False, squeeze_me=True)
+        return MatlabFuncs._check_keys(data)
+
+    @staticmethod
+    def _check_keys(d: dict) -> dict:
+        for key in d:
+            if isinstance(d[key], spio.matlab.mio5_params.mat_struct):
+                d[key] = MatlabFuncs._todict(d[key])
+        return d
+
+    @staticmethod
+    def _todict(matobj: spio.matlab.mio5_params.mat_struct) -> dict:
+        d = {}
+        for strg in matobj._fieldnames:
+            elem = matobj.__dict__[strg]
+            if isinstance(elem, spio.matlab.mio5_params.mat_struct):
+                d[strg] = MatlabFuncs._todict(elem)
+            else:
+                d[strg] = elem
+        return d
