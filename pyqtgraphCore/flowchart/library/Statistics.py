e2745bf36c41542b3cb1c6b46c6de2ee87314d51
kushal <kushal@kushal-ThinkPad-P52s>
2019-03-09 06:40:42 +0100

    Reorganization and bug fixes. Update Definitions.py, Display.py, and 2 more files...

--- pyqtgraphCore/flowchart/library/Statistics.py
+++ pyqtgraphCore/flowchart/library/Statistics.py
@@ -12,95 +12,15 @@ from ... import metaarray as metaarray
 from analyser.DataTypes import *
 from functools import partial
 from analyser import PeakEditor
-from analyser import Extraction
+from analyser import peak_feature_extraction
 from analyser.stats_gui import StatsWindow
-from plotting.old.beeswarms_window import BeeswarmPlotWindow
 from plotting.old.curve_plots_window import CurvePlotWindow
 from analyser import pca_gui
 import pickle
 import traceback
-from clustering.LDA.main_window import LDAPlot
 
 
-def _open_plot_gui(self):
-    if self.plot_gui is None:
-        self.plot_gui = BeeswarmPlotWindow(parent=self.parent())
-    self.plot_gui.show()
 
-
-class BeeswarmPlots(CtrlNode):
-    """Beeswarm and Violin plots"""
-    nodeName = 'BeeswarmPlots'
-    uiTemplate = [('Apply', 'check', {'checked': False, 'applyBox': True}),
-                  ('ShowGUI', 'button', {'text': 'OpenGUI'})]
-
-    def __init__(self, name):
-        CtrlNode.__init__(self, name, terminals={'In': {'io': 'in', 'multi': True}})
-        self.plot_gui = None
-        self.ctrls['ShowGUI'].clicked.connect(self._open_plot_gui)
-
-    def process(self, **kwargs):
-        if (self.ctrls['Apply'].isChecked() is False) or self.plot_gui is None:
-            return
-
-        transmissions = kwargs['In']
-        transmissions_list = merge_transmissions(transmissions)
-
-        # transmissions = kwargs['In']
-        #
-        # if not len(transmissions) > 0:
-        #     raise Exception('No incoming transmissions')
-        #
-        # transmissions_list = []
-        #
-        # for t in transmissions.items():
-        #     t = t[1]
-        #     if t is None:
-        #         QtWidgets.QMessageBox.warning(None, 'None transmission', 'One of your transmissions is None')
-        #         continue
-        #     if type(t) is list:
-        #         for i in range(len(t)):
-        #             if t[i] is None:
-        #                 QtWidgets.QMessageBox.warning(None, 'None transmission', 'One of your transmissions is None')
-        #                 continue
-        #             transmissions_list.append(t[i].copy())
-        #         continue
-        #
-        #     transmissions_list.append(t.copy())
-
-        self.plot_gui.update_input_transmissions(transmissions_list)
-
-    def _open_plot_gui(self):
-        if self.plot_gui is None:
-            self.plot_gui = BeeswarmPlotWindow(parent=self.parent())
-        self.plot_gui.show()
-
-
-class LDA(CtrlNode):
-    """LDA"""
-    nodeName = 'LDA'
-    uiTemplate = [('Apply', 'check', {'checked': False, 'applyBox': True}),
-                  ('ShowGUI', 'button', {'text': 'OpenGUI'})]
-
-    def __init__(self, name):
-        CtrlNode.__init__(self, name, terminals={'In': {'io': 'in', 'multi': True}})
-        self.plot_gui = None
-        self.ctrls['ShowGUI'].clicked.connect(self._open_plot_gui)
-
-    def process(self, **kwargs):
-        if (self.ctrls['Apply'].isChecked() is False) or self.plot_gui is None:
-            return
-
-        transmissions = kwargs['In']
-
-        transmissions_list = merge_transmissions(transmissions)
-
-        self.plot_gui.update_input_transmissions(transmissions_list)
-
-    def _open_plot_gui(self):
-        if self.plot_gui is None:
-            self.plot_gui = LDAPlot(parent=self.parent())
-        self.plot_gui.show()
 
 #
 # class StackTraces(CtrlNode):
@@ -179,11 +99,11 @@ class CurvePlotsNode(CtrlNode):
         self.plot_gui.show()
 
 
-class PeakFeaturesExtract(CtrlNode):
+class PeakFeatures(CtrlNode):
     """Extract peak features. Use this after the Peak_Detect node. This node does not operate live, you must
     click the "Extract" button to propogate newly computed peak features"""
     nodeName = 'Peak_Features'
-    uiTemplate = [('Extract', 'button', {'text': 'Compute'}),
+    uiTemplate = [('Compute', 'button', {'text': 'Compute'}),
                   ('Info', 'label', {'text': ''})]
 
     # uiTemplate = [('Extract', 'button', {'text': 'Compute'}),
@@ -192,7 +112,7 @@ class PeakFeaturesExtract(CtrlNode):
 
     def __init__(self, name):
         CtrlNode.__init__(self, name, terminals={'In': {'io': 'in', 'multi': True}, 'Out': {'io': 'out', 'bypass': 'In'}})
-        self.ctrls['Extract'].clicked.connect(self._extract)
+        self.ctrls['Compute'].clicked.connect(self._compute)
         # self.ctrls['Stats'].setEnabled(False)
         # self.ctrls['Stats'].clicked.connect(self._open_stats_gui)
         self.peak_results = None
@@ -201,7 +121,7 @@ class PeakFeaturesExtract(CtrlNode):
         self.kwargs = kwargs.copy()
         return {'Out': self.peak_results}
 
-    def _extract(self):
+    def _compute(self):
         if self.kwargs is None:
             self.peak_results = None
             return
@@ -223,13 +143,13 @@ class PeakFeaturesExtract(CtrlNode):
             # t = t.copy()
             try:
                 self.ctrls['Info'].setText('Please wait, computing peak features')
-                pf = Extraction.PeakFeaturesIter(t)
+                pf = peak_feature_extraction.PeakFeaturesIter(t)
                 trans_with_features = pf.get_all()
 
                 self.peak_results.append(trans_with_features)
 
             except Exception as e:
-                QtWidgets.QMessageBox.warning(None, 'Error computing', 'The following error occured during peak extraction:\n'
+                QtWidgets.QMessageBox.warning(None, 'Error computing', 'The following error occured during peak feature extraction:\n'
                                                                    + traceback.format_exc())
 
         self.ctrls['Info'].setText('Finished computing peak features!')
