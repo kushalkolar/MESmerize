31f052e2457a01f5316848d8c3352d626d9b351f
danieldondorp <danieldondorp@gmail.com>
2019-05-15 16:25:21 +0200

    Added label rotation as parameter of the ViolinsPlot.set() method. Default value 45

--- plotting/variants/violins.py
+++ plotting/variants/violins.py
@@ -9,42 +9,44 @@ Sars International Centre for Marine Molecular Biology
 GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 """
 
-#from pyqtgraphCore.widgets import MatplotlibWidget as MatplotlibWidget
-#from pyqtgraphCore.widgets.MatplotlibWidget import MatplotlibWidget
+from pyqtgraphCore.widgets.MatplotlibWidget import MatplotlibWidget
 import seaborn as sns
+import matplotlib.pyplot as plt
 from math import ceil
 
 
 class ViolinsPlot(MatplotlibWidget):
     def __init__(self):
-        MatplotlibWidget.__init__(self)
-        # sns.set()
-        self.fig.subplots_adjust(hspace=0.3, wspace=0.3)
+        MatplotlibWidget.__init__(self, size = (8,6), dpi=100)
+        sns.set()
+#        self.fig.subplots_adjust(hspace=0.3, wspace=0.3)
         self.num_columns = 4
         self.axes = []
 
-    def set(self, sub_dataframe, data_columns: list, x_column: str, sub_group_column: str = None, x_order: list = None):
+    def set(self, sub_dataframe, data_columns: list, x_column: str, sub_group_column: str = None, x_order: list = None, label_rotation: int = 45):
         self.axes = []
         self.fig.clear()
 
         num_rows = ceil(len(data_columns) / self.num_columns)
 
         for i, data_column in enumerate(data_columns):
-            self.axes.append(self.fig.add_subplot(num_rows, self.num_columns, i + 1))
+            ax = self.fig.add_subplot(num_rows, self.num_columns, i + 1)
+            self.axes.append(ax)
             sns.violinplot(x=x_column, y=data_column, hue=sub_group_column, order=x_order, palette='muted',
                                 data=sub_dataframe[sub_dataframe[data_column].notna()], ax=self.axes[i])
-
-            self.axes.set_xticklabels(self.axes.get_xticklabels(), rotation = 30)
+            ax.set_xticklabels(list(sub_dataframe[x_column].unique()), rotation=label_rotation)
+        self.fig.tight_layout()
         self.draw()
 
+##Testing code
 if __name__ == "__main__":
     import pandas as pd
 
     import sys
     from PyQt5 import QtWidgets
     iris = sns.load_dataset('iris')
-    app = QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication([])
     v = ViolinsPlot()
-    v.set(iris, ["versicolor", "setosa", "virginica"], "sepal_length")
+    v.set(iris, [x for x in iris.columns if "species" not in x], "species", label_rotation=90)
     v.show()
     sys.exit(app.exec())
