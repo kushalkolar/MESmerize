75961eee2c0a312262bf7af0dd92ecce9932a50d
kushalkolar <kushalkolar@gmail.com>
2019-06-04 16:14:54 +0200

    Began sum of squared diff between rfft and irfft GUI. Update sosd.py, __init__.py, and 3 more files...

---
+++ mesmerize/plotting/widgets/sosd_fourier/widget.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on July 7 2018
+
+@author: kushal
+
+Chatzigeorgiou Group
+Sars International Centre for Marine Molecular Biology
+
+GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+"""
+
+from .template import *
+import numpy as np
+
