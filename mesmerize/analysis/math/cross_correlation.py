e5c904aa9b4080414bd5ee106491705625e2f0f9
kushalkolar <kushalkolar@gmail.com>
2019-06-04 08:36:33 +0200

    Add some doc strings to analysis.math.cross_correlation. Cleanup a bit in the root dir. Update Amazing animal.mes, MANIFEST.in, and 3 more files...

--- mesmerize/analysis/math/cross_correlation.py
+++ mesmerize/analysis/math/cross_correlation.py
@@ -11,16 +11,29 @@ GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 
 import numpy as np
 from tslearn.cycc import normalized_cc
-from tslearn.preprocessing import TimeSeriesScalerMeanVariance, TimeSeriesScalerMinMax
+# from tslearn.preprocessing import TimeSeriesScalerMeanVariance, TimeSeriesScalerMinMax
 # from sklearn.metrics import pairwise_distances
 
 
 def ncc_c(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """
+    :param x: Input vector
+    :param y: Input vector
+    :return:  Returns the normalized cross correlation function (as a vector) of two input vectors
+    """
     cc = normalized_cc(x.reshape(-1, 1), y.reshape(-1, 1))
     return cc
 
 
 def get_omega(x: np.ndarray = None, y: np.ndarray = None, cc: np.ndarray = None) -> int:
+    """
+    Must pass a 1D vector to either both "x" and "y" or a cross-correlation function to "cc"
+
+    :param x: Input vector
+    :param y: Input vector
+    :param cc: cross-correlation function represented as a vector
+    :return: index (x-axis position) of the global maxima of the cross-correlation function
+    """
     if cc is None:
         cc = ncc_c(x, y)
     w = np.argmax(cc)
@@ -28,6 +41,14 @@ def get_omega(x: np.ndarray = None, y: np.ndarray = None, cc: np.ndarray = None)
 
 
 def get_lag(x: np.ndarray = None, y: np.ndarray = None, cc: np.ndarray = None) -> float:
+    """
+    Must pass a 1D vector to either both "x" and "y" or a cross-correlation function to "cc"
+
+    :param x: Input vector
+    :param y: Input vector
+    :param cc: cross-correlation function represented as a vector
+    :return: Position of the maxima of the cross-correlation function with respect to middle point of the cross-correlation function
+    """
     if cc is None:
         w = get_omega(x, y)
         s = w - x.shape[0]
@@ -38,6 +59,14 @@ def get_lag(x: np.ndarray = None, y: np.ndarray = None, cc: np.ndarray = None) -
 
 
 def get_epsilon(x: np.ndarray = None, y: np.ndarray = None, cc: np.ndarray = None) -> float:
+    """
+    Must pass a 1D vector to either both "x" and "y" or a cross-correlation function to "cc"
+
+    :param x: Input vector
+    :param y: Input vector
+    :param cc: cross-correlation function represented as a vector
+    :return: Magnitude of the global maxima of the cross-correlationn function
+    """
     if cc is None:
         cc = ncc_c(x, y)
     e = np.max(cc)
