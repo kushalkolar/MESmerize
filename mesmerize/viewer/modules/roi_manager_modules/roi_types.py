aace9e41895133862943b7ce31121c08e90bb9bb
kushalkolar <kushalkolar@gmail.com>
2019-06-06 22:11:06 +0200

    Fixing bugs due to new window manager and sys config.

--- mesmerize/viewer/modules/roi_manager_modules/roi_types.py
+++ mesmerize/viewer/modules/roi_manager_modules/roi_types.py
@@ -13,7 +13,7 @@ GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 import abc
 import numpy as np
 from PyQt5 import QtWidgets
-from  .... import pyqtgraphCore as pg
+from .... import pyqtgraphCore as pg
 # from typing import Type, TypeVar
 from ....common import get_proj_config
 from copy import deepcopy
