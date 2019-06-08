04c339bb4fafe98e8bf0b26905392297944891af
kushalkolar <kushalkolar@gmail.com>
2019-06-08 03:56:00 +0200

    Added to unittests Update batch_manager.py, viewer.py, and welcome_window.py

--- tests/viewer.py
+++ tests/viewer.py
@@ -1,29 +1,62 @@
 import unittest
-from .welcome_window import TestViewerLaunch, get_window_manager
+from .welcome_window import TestViewerLaunch, get_window_manager, App, data_dir
 from mesmerize.viewer.modules.tiff_io import ModuleGUI as TiffModuleGUI
 from PyQt5.QtTest import QTest
 from PyQt5.QtCore import Qt
 import os
+import tifffile
+import json
+import numpy
+
 
 class ImageIOModules(TestViewerLaunch):
     def test_tiff_module(self):
-        viewer_window = get_window_manager().viewer[-1]
+        viewer_window = get_window_manager().viewers[-1]
         for m in viewer_window.running_modules:
             self.assertNotIsInstance(m, TiffModuleGUI, 'Tiff module already running after viewer launcher')
-        viewer_window.actionTiff_file.trigger()
+        viewer_window.ui.actionTiff_file.trigger()
 
         if not any(isinstance(m, TiffModuleGUI) for m in viewer_window.running_modules):
             self.fail('Tiff module not appended to running modules of viewer window')
 
-        tm = viewer_window.get_module('tiff_io')
+        tm = viewer_window.run_module('tiff_io')
 
         self.assertIsInstance(tm, TiffModuleGUI, 'Getting Tiff by str reference failed')
+        assert isinstance(tm, TiffModuleGUI)
+
+        QTest.qWaitForWindowExposed(tm)
 
         # Check tiff module defaults
         self.assertFalse(tm.ui.radioButtonImread.isChecked())
         self.assertFalse(tm.ui.radioButtonAsarray.isChecked())
-        QTest.mouseClick(tm.ui.radioButtonImread, Qt.LeftButton)
+        tm.ui.radioButtonImread.click()
+        self.assertTrue(tm.ui.radioButtonImread.isChecked())
 
-        data_dir = os.path.join(os.path.abspath(__file__), 'data')
+        tiff_file = os.path.join(data_dir, 'tiff', 'test_imread.tiff')
+        json_file = os.path.join(data_dir, 'tiff', 'test_imread.json')
 
-        
+        tm._tiff_file_path = tiff_file
+
+        tm.check_meta_path()
+
+        # Check that meta data is deteted
+        self.assertEqual(tm._meta_file_path, json_file)
+
+        # Load image
+        QTest.mouseClick(tm.ui.btnLoadIntoWorkEnv, Qt.LeftButton)
+
+        # Check the image arrays
+        seq = tifffile.imread(tiff_file)
+
+        numpy.testing.assert_array_equal(seq, viewer_window.viewer_reference.workEnv.imgdata.seq.T, 'Output of tifffile.imread does not match ViewerWorkEnv.ImgData.seq.T')
+        numpy.testing.assert_array_equal(seq, viewer_window.viewer_reference.image, 'ImageView.image array does not equal output of tifffile.imread')
+
+        # Check the meta data file
+        meta = viewer_window.viewer_reference.workEnv.imgdata.meta
+        self.assertFalse({} == meta, "ViewerWorkEnv dict appears to be empty")
+        print(f"ViewerWorkEnv meta data dict\n{meta}")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/welcome_window.py
+++ tests/welcome_window.py
@@ -6,21 +6,33 @@ from mesmerize.common import get_window_manager, get_project_manager
 from mesmerize.viewer import ViewerWindow
 from mesmerize.common.welcome_window import MainWindow
 from mesmerize.analysis.flowchart import Window as FlowchartWindow
+import os
+
+data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
 app = App([])
 
+class BaseWelcomeWindowTest(unittest.TestCase):
+    # @classmethod
+    # def setup(cls):
+    #     cls.welcome_window = MainWindow()
+    #     QTest.qWaitForWindowExposed(cls.welcome_window)
 
-class WelcomeWindowTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        if cls is BaseWelcomeWindowTest:
+            raise unittest.SkipTest("Skipping BaseWelcomeWindowTest")
+
         cls.welcome_window = MainWindow()
+        get_window_manager().welcome_window = cls.welcome_window
         QTest.qWaitForWindowExposed(cls.welcome_window)
+        super(BaseWelcomeWindowTest, cls).setUpClass()
 
     # def test_launch_welcome_window(self):
     #     self.welcome_window.show()
 
 
-class TestViewerLaunch(WelcomeWindowTest):
+class TestViewerLaunch(BaseWelcomeWindowTest):
     def test_launch_viewer(self):
         num_viewers = len(get_window_manager().viewers)
         # QtTest.QTest.qWaitForWindowExposed(self.welcome_window)
@@ -31,7 +43,7 @@ class TestViewerLaunch(WelcomeWindowTest):
         QTest.qWaitForWindowExposed(get_window_manager().viewers[-1])
 
 
-class TestFlowchartLauncher(WelcomeWindowTest):
+class TestFlowchartLauncher(BaseWelcomeWindowTest):
     def test_launch_flowchart(self):
         num_flowcharts = len(get_window_manager().flowcharts)
         QTest.mouseClick(self.welcome_window.ui.btnFlowchart, Qt.LeftButton)
