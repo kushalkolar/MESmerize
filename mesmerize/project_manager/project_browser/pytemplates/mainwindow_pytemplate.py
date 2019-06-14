edeb4f43964ea8e2b1a450ecc93214411666bf5c
kushalkolar <kushalkolar@gmail.com>
2019-06-14 11:40:45 +0200

    Update mainwindow_pytemplate.py and mainwindow.ui

--- mesmerize/project_manager/project_browser/pytemplates/mainwindow_pytemplate.py
+++ mesmerize/project_manager/project_browser/pytemplates/mainwindow_pytemplate.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainwindow.ui'
+# Form implementation generated from reading ui file './ui_files/mainwindow.ui'
 #
-# Created by: PyQt5 UI code generator 5.6
+# Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -16,7 +16,7 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 30))
         self.menubar.setObjectName("menubar")
         self.menuDataframe = QtWidgets.QMenu(self.menubar)
         self.menuDataframe.setObjectName("menuDataframe")
@@ -26,12 +26,14 @@ class Ui_MainWindow(object):
         self.menuSave_all_tabs.setObjectName("menuSave_all_tabs")
         self.menuView = QtWidgets.QMenu(self.menubar)
         self.menuView.setObjectName("menuView")
+        self.menuTools = QtWidgets.QMenu(self.menubar)
+        self.menuTools.setObjectName("menuTools")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
         self.dockConsole = QtWidgets.QDockWidget(MainWindow)
-        self.dockConsole.setMinimumSize(QtCore.QSize(42, 200))
+        self.dockConsole.setMinimumSize(QtCore.QSize(68, 200))
         self.dockConsole.setFeatures(QtWidgets.QDockWidget.DockWidgetFloatable|QtWidgets.QDockWidget.DockWidgetMovable)
         self.dockConsole.setObjectName("dockConsole")
         self.dockWidgetContents = QtWidgets.QWidget()
@@ -61,6 +63,10 @@ class Ui_MainWindow(object):
         self.actionto_excel.setObjectName("actionto_excel")
         self.actionto_csv = QtWidgets.QAction(MainWindow)
         self.actionto_csv.setObjectName("actionto_csv")
+        self.actionLink_viewers = QtWidgets.QAction(MainWindow)
+        self.actionLink_viewers.setObjectName("actionLink_viewers")
+        self.actionSave_to_project = QtWidgets.QAction(MainWindow)
+        self.actionSave_to_project.setObjectName("actionSave_to_project")
         self.menuSave_current_tab.addSeparator()
         self.menuSave_current_tab.addAction(self.actionto_pickle_tab)
         self.menuSave_current_tab.addAction(self.actionto_csv_tab)
@@ -75,11 +81,15 @@ class Ui_MainWindow(object):
         self.menuDataframe.addSeparator()
         self.menuDataframe.addAction(self.menuSave_current_tab.menuAction())
         self.menuDataframe.addAction(self.menuSave_all_tabs.menuAction())
+        self.menuDataframe.addSeparator()
+        self.menuDataframe.addAction(self.actionSave_to_project)
         self.menuView.addAction(self.actionCurrent_tab_filter_history)
         self.menuView.addSeparator()
         self.menuView.addAction(self.actionConsole)
+        self.menuTools.addAction(self.actionLink_viewers)
         self.menubar.addAction(self.menuDataframe.menuAction())
         self.menubar.addAction(self.menuView.menuAction())
+        self.menubar.addAction(self.menuTools.menuAction())
 
         self.retranslateUi(MainWindow)
         self.actionConsole.toggled['bool'].connect(self.dockConsole.setVisible)
@@ -88,21 +98,24 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.menuDataframe.setTitle(_translate("MainWindow", "Dataframe"))
-        self.menuSave_current_tab.setTitle(_translate("MainWindow", "Save current tab"))
-        self.menuSave_all_tabs.setTitle(_translate("MainWindow", "Save root"))
-        self.menuView.setTitle(_translate("MainWindow", "View"))
-        self.dockConsole.setWindowTitle(_translate("MainWindow", "Console: Project Browser"))
-        self.actionDataframe_editor.setText(_translate("MainWindow", "Dataframe editor"))
-        self.actionConsole.setText(_translate("MainWindow", "Console"))
-        self.actionCurrent_tab_filter_history.setText(_translate("MainWindow", "Current tab filter history"))
-        self.actionUpdate_current_tab.setText(_translate("MainWindow", "Update current tab"))
-        self.actionUpdate_all_tabs.setText(_translate("MainWindow", "Update all tabs"))
-        self.actionto_pickle_tab.setText(_translate("MainWindow", "to pickle"))
+        self.menuDataframe.setTitle(_translate("MainWindow", "Dataf&rame"))
+        self.menuSave_current_tab.setTitle(_translate("MainWindow", "&Save current tab"))
+        self.menuSave_all_tabs.setTitle(_translate("MainWindow", "Export root"))
+        self.menuView.setTitle(_translate("MainWindow", "&View"))
+        self.menuTools.setTitle(_translate("MainWindow", "Too&ls"))
+        self.dockConsole.setWindowTitle(_translate("MainWindow", "&Console: Project Browser"))
+        self.actionDataframe_editor.setText(_translate("MainWindow", "&Dataframe editor"))
+        self.actionConsole.setText(_translate("MainWindow", "C&onsole"))
+        self.actionCurrent_tab_filter_history.setText(_translate("MainWindow", "&Current tab filter history"))
+        self.actionUpdate_current_tab.setText(_translate("MainWindow", "Update &current tab"))
+        self.actionUpdate_all_tabs.setText(_translate("MainWindow", "&Update all tabs"))
+        self.actionto_pickle_tab.setText(_translate("MainWindow", "&to pickle"))
         self.actionto_pickle_tab.setToolTip(_translate("MainWindow", "Save current tab as a python pickle"))
-        self.actionto_csv_tab.setText(_translate("MainWindow", "to csv"))
-        self.actionto_excel_tab.setText(_translate("MainWindow", "to excel"))
-        self.actionto_pickle.setText(_translate("MainWindow", "to pickle"))
-        self.actionto_excel.setText(_translate("MainWindow", "to excel"))
-        self.actionto_csv.setText(_translate("MainWindow", "to csv"))
+        self.actionto_csv_tab.setText(_translate("MainWindow", "to &csv"))
+        self.actionto_excel_tab.setText(_translate("MainWindow", "to &excel"))
+        self.actionto_pickle.setText(_translate("MainWindow", "&to pickle"))
+        self.actionto_excel.setText(_translate("MainWindow", "to &excel"))
+        self.actionto_csv.setText(_translate("MainWindow", "to &csv"))
+        self.actionLink_viewers.setText(_translate("MainWindow", "&Link viewers"))
+        self.actionSave_to_project.setText(_translate("MainWindow", "Save to project"))
 
