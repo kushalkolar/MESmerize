26e87b273dc781a4ca90eaec46b26b4a6591beaa
kushalkolar <kushalkolar@gmail.com>
2019-06-27 09:51:19 +0200

    Began implementing decorators for common QMessageBoxes and QFileDialogs. Update __init__.py, welcome_window.py, and 2 more files...

--- mesmerize/common/__init__.py
+++ mesmerize/common/__init__.py
@@ -1,5 +1,5 @@
 import os
-from webbrowser import open_new_tab
+from webbrowser import open_new_tab as open_new_web_browser_tab
 import configparser
 from datetime import datetime
 from time import time
@@ -7,7 +7,10 @@ from time import time
 from .. import docs
 from .configuration import get_sys_config
 from functools import partial
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMessageBox, QFileDialog
+from functools import wraps
+import traceback
+
 
 # def get_sys_config() -> configparser.RawConfigParser:
 #     sys_cfg_file = os.environ['HOME'] + '/.mesmerize'
@@ -43,7 +46,7 @@ doc_pages = {'home':            '/../index.html',
              }
 
 for k in doc_pages.keys():
-    doc_pages[k] = partial(open_new_tab, docs_dir + doc_pages[k])
+    doc_pages[k] = partial(open_new_web_browser_tab, docs_dir + doc_pages[k])
 
 
 def get_timestamp_str() -> str:
@@ -82,6 +85,33 @@ def get_window_manager():
     # except AttributeError:
     #     raise AttributeError("This can only be used in a full Mesmerize Application")
     # return wm
+
+
+def error_window(title: str = 'error', message: str = 'The following error occurred.'):
+    def catcher(func):
+        @wraps(func)
+        def fn(self, *args, **kwargs):
+            try:
+                func(self, *args, **kwargs)
+            except:
+                QMessageBox.warning(self, title, message + f'\n\n{traceback.format_exc()}')
+                # raise
+        return fn
+    return catcher
+
+
+def get_file_dialog(title: str = 'Choose file', start_dir: str = '', exts: list = []):
+    def wrapper(func):
+        @wraps(func)
+        def fn(self, *args, **kwargs):
+            path = QFileDialog.getOpenFileName(self, title, start_dir, f'{" ".join(exts)}')
+            if path == '':
+                return
+            if path[0] == '':
+                return
+            func(self, path[0])#, *args, **kwargs)
+        return fn
+    return wrapper
 
 
 class NoProjectOpen(BaseException):
