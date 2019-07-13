f6783be8938f87207a97345981af101085272efb
kushalkolar <kushalkolar@gmail.com>
2019-07-13 21:14:18 +0200

    Reorganization of nodes 	modified:   mesmerize/pyqtgraphCore/flowchart/library/Data.py 	modified:   mesmerize/pyqtgraphCore/flowchart/library/Definitions.py 	modified:   mesmerize/pyqtgraphCore/flowchart/library/Display.py 	modified:   mesmerize/pyqtgraphCore/flowchart/library/Hierarchical.py 	modified:   mesmerize/pyqtgraphCore/flowchart/library/Math.py 	modified:   mesmerize/pyqtgraphCore/flowchart/library/Signal.py 	modified:   mesmerize/pyqtgraphCore/flowchart/library/Statistics.py 	modified:   mesmerize/pyqtgraphCore/flowchart/library/__init__.py

--- mesmerize/pyqtgraphCore/flowchart/library/Math.py
+++ mesmerize/pyqtgraphCore/flowchart/library/Math.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from ....analysis.math.tvregdiff import tv_reg_diff
 from .common import *
 from ....analysis.data_types import Transmission
-from scipy import signal
-from functools import partial
-from scipy import fftpack
-import pandas as pd
 
 
 class AbsoluteValue(CtrlNode):
@@ -102,14 +99,41 @@ class LogTransform(CtrlNode):
         return self.t
 
 
-class RFFT(CtrlNode):
-    """
-    Uses fftpack.rfft, 'Discrete Fourier transform of a real sequence.\n
-    https://docs.scipy.org/doc/scipy/reference/generated/scipy.fftpack.rfft.html#scipy.fftpack.rfft
-    """
-    nodeName = 'RFFT'
+class Derivative(CtrlNode):
+    """Return the Derivative of a curve."""
+    nodeName = 'Derivative'
     uiTemplate = [('data_column', 'combo', {}),
+                  ('dt', 'intSpin', {'min': 1, 'max': 999, 'value': 1, 'step': 1}),
                   ('Apply', 'check', {'checked': False, 'applyBox': True})
+                  ]
+
+    def processData(self, transmission: Transmission):
+        self.t = transmission
+        self.set_data_column_combo_box()
+        if self.ctrls['Apply'].isChecked() is False:
+            return
+
+        self.t = transmission.copy()
+
+        output_column = '_DERIVATIVE'
+
+        self.t.df[output_column] = self.t.df[self.data_column].apply(np.gradient)
+        self.t.last_output = output_column
+
+        params = {'data_column': self.data_column,
+                  'units': self.t.last_unit
+                  }
+
+        self.t.history_trace.add_operation(data_block_id='all', operation='derivative', parameters=params)
+
+        return self.t
+
+
+class TVDiff(CtrlNode):
+    """Total Variation Regularized Numerical Differentiation, Chartrand 2011 method"""
+    nodeName = 'TVDiff'
+    uiTemplate = [('data_column', 'combo', {}),
+                  ('Apply', 'check', {'checked': False, 'applyBox': True}),
                   ]
 
     def processData(self, transmission: Transmission):
@@ -119,85 +143,20 @@ class RFFT(CtrlNode):
         if self.ctrls['Apply'].isChecked() is False:
             return
 
-        # TODO: Replace this code block with the DataTypes.get_framerate() method
-        ##############################################################################################################
-        sampling_rates = []
-
-        for db in transmission.history_trace.data_blocks:
-            if transmission.history_trace.check_operation_exists(db, 'resample'):
-                sampling_rates.append(transmission.history_trace.get_operation_params(db, 'resample')['output_rate'])
-            else:
-                r = pd.DataFrame(transmission.get_data_block_dataframe(db).meta.to_list())['fps'].unique()
-                # if rates.size > 1:
-                #     raise ValueError("Sampling rates for the data do not match")
-                # else:
-                sampling_rates.append(r)
-
-        rates = np.hstack([sampling_rates])
-
-        if np.ptp(rates) > 0.1:
-            raise ValueError("Sampling rates of the data differ by greater than the tolerance of 0.1 Hz")
-
-        framerate = int(np.mean(sampling_rates))
-        ##############################################################################################################
-
-        array_size = transmission.df[self.data_column].apply(lambda a: a.size).unique()
-
-        if array_size.size > 1:
-            raise ValueError("Size of all arrays in data column must match exactly.")
-
-        array_size = array_size[0]
-
-        freqs = fftpack.rfftfreq(array_size) * framerate
-
         self.t = transmission.copy()
 
-        output_column = '_RFFT'
+        output_column = '_TVDIFF'
+        tqdm.pandas()
+        self.t.df[output_column] = self.t.df[self.data_column].progress_apply(lambda x: self._func(x, 100, 1e-1, dx=0.05, ep=1e-2, scale='large', diagflag=0))
+        self.t.last_output = output_column
 
-        self.t.df[output_column] = self.t.df[self.data_column].apply(fftpack.rfft)
-
-        self.t.last_unit = 'frequency'
-
-        params = {'data_column':    self.data_column,
-                  'frequencies':    freqs.tolist(),
-                  'sampling_rate':  framerate,
-                  'nyquist_frequency': float(freqs.max()),
-                  'units': 'frequency'
+        params = {'data_column': self.data_column,
+                  'units': self.t.last_unit
                   }
 
-        self.t.history_trace.add_operation(data_block_id='all', operation='rfft', parameters=params)
-        self.t.last_output = output_column
+        self.t.history_trace.add_operation(data_block_id='all', operation='tvdiff', parameters=params)
 
         return self.t
 
-
-class iRFFT(CtrlNode):
-    """
-    Uses fftpack.irfft, 'Return inverse discrete Fourier transform of real sequence.'\n
-    https://docs.scipy.org/doc/scipy/reference/generated/scipy.fftpack.irfft.html#scipy.fftpack.irfft\n
-    Input must have an _RFFT column from the RFFT node.\n
-    """
-    nodeName = 'iRFFT'
-    uiTemplate = [('Apply', 'check', {'checked': False, 'applyBox': True})]
-
-    def processData(self, transmission: Transmission):
-        self.t = transmission
-        self.set_data_column_combo_box()
-        if self.ctrls['Apply'].isChecked() is False:
-            return
-
-        self.t = transmission.copy()
-
-        output_column = '_IRFFT'
-
-        self.t.df[output_column] = self.t.df['_RFFT'].apply(fftpack.irfft)
-        self.t.last_unit = 'time'
-
-        params = {'data_column': '_RFFT',
-                  'units': 'time'
-                  }
-
-        self.t.history_trace.add_operation(data_block_id='all', operation='irfft', parameters=params)
-        self.t.last_output = output_column
-
-        return self.t
+    def _func(self, *args, **kwargs):
+        return tv_reg_diff(*args, **kwargs)
