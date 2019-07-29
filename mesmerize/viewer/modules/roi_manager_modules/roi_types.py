cbaf038317e463f16d6a24d81d7d9a390949b235
kushalkolar <kushalkolar@gmail.com>
2019-07-29 02:56:28 +0200

    Added some docstrings to the ROI classes. Update roi_manager.py, managers.py, and roi_types.py

--- mesmerize/viewer/modules/roi_manager_modules/roi_types.py
+++ mesmerize/viewer/modules/roi_manager_modules/roi_types.py
@@ -21,84 +21,171 @@ from copy import deepcopy
 from typing import Union
 
 
-class AbstractBaseROI(metaclass=abc.ABCMeta):
+class _InheritDocs(abc.ABCMeta):
+    def __new__(mcls, classname, bases, cls_dict):
+        cls = super().__new__(mcls, classname, bases, cls_dict)
+        for name, member in cls_dict.items():
+            if not getattr(member, '__doc__'):
+                if hasattr(bases[-1], name):
+                    member.__doc__ = getattr(bases[-1], name).__doc__
+        return cls
+
+
+class _AbstractBaseROI(metaclass=_InheritDocs):
+    """
+    Abstract base class defining an ROI that works with the ROIList and ROI Managers.
+    Inherit from this or BaseROI to make a new ROI class
+    """
     @abc.abstractmethod
-    def __init__(self, curve_plot_item: pg.PlotDataItem, view_box: pg.ViewBox, state: dict):
+    def __init__(self, curve_plot_item: pg.PlotDataItem, view_box: pg.ViewBox, state: Union[dict, None]):
+        """
+        Minimum required attributes
+
+        :param curve_plot_item: The plot item that is used for display the curves in the viewer
+        :param view_box:        ViewBox containing the image sequence, used for overlaying the ROIs on top of the image
+        :param state:           ROI state, used for restoring the ROIs. Pass None is not restoring an ROI from a state dict
+        """
         pass
 
     @property
     @abc.abstractmethod
-    def curve_data(self) -> list:
+    def curve_data(self) -> tuple:
+        """
+        The curve data for this ROI
+
+        :return: (x, y), [np.ndarray, np.ndarray]
+        :rtype: tuple
+        """
         pass
 
     @curve_data.setter
     @abc.abstractmethod
-    def curve_data(self, data: list):
+    def curve_data(self, data: tuple):
         """
-        :param data: [x, y], [np.ndarray, np.ndarray]
+        Set the curve data for this ROI
+
+        :param data: (x, y), [np.ndarray, np.ndarray]
         """
         pass
 
     @abc.abstractmethod
     def get_roi_graphics_object(self) -> QtWidgets.QGraphicsObject:
+        """Get the QGraphicsObject used for visualization of the spatial localization of the ROI"""
         pass
 
     @abc.abstractmethod
     def set_roi_graphics_object(self, *args, **kwargs):
+        """Set the QGraphicsObject used for visualization of the spatial localization of the ROI"""
         pass
 
     @abc.abstractmethod
     def reset_color(self):
+        """Reset the color of this ROI back to the original color"""
         pass
 
     @abc.abstractmethod
     def set_original_color(self, color):
+        """
+        Set the original color for this ROI
+
+        :param color: 1D numpy array of 4 floating point numbers (range 0 - 255) in RBGA format, [R, G, B, A]
+        """
         pass
 
     @abc.abstractmethod
-    def get_color(self):
+    def get_color(self) -> np.ndarray:
+        """
+        Get the current color of this ROI
+
+        :return: 1D numpy array of 4 floating point numbers (range 0 - 255) in RBGA format, [R, G, B, A]
+        :rtype: np.ndarray
+        """
         pass
 
     @abc.abstractmethod
     def set_color(self, color, *args, **kwargs):
+        """
+        Set the current color of this ROI
+
+        :param color: 1D numpy array of 4 floating point numbers (range 0 - 255) in RBGA format, [R, G, B, A]
+        """
         pass
 
     @abc.abstractmethod
     def set_text(self, text: str):
+        """Not implemented"""
         pass
 
     @abc.abstractmethod
     def set_tag(self, roi_def: str, tag: str):
+        """
+        Set a tag for the passed roi_def
+
+        :param roi_def: The ROI_DEF that should be tagged
+        :param tag:     The tag to label for the passed ROI_DEF/ROI Type
+        """
         pass
 
     @abc.abstractmethod
     def get_tag(self, roi_def) -> str:
+        """
+        Get the tag that is set to the passed 'roi_def'
+
+        :rtype: str
+        """
         pass
 
     @abc.abstractmethod
     def get_all_tags(self) -> dict:
+        """
+        Get all the tags for all the ROI_DEFs
+
+        :rtype: dict
+        """
         pass
 
     @abc.abstractmethod
     def add_to_viewer(self):
+        """Add this ROI to the viewer."""
         pass
 
     @abc.abstractmethod
     def remove_from_viewer(self):
+        """Remove this ROI from the viewer"""
         pass
 
     @abc.abstractmethod
     def to_state(self):
+        """Get the current state for this ROI so that it can be restored later"""
         pass
 
     @classmethod
     @abc.abstractmethod
     def from_state(cls, curve_plot_item: pg.PlotDataItem, view_box: pg.ViewBox, state: dict):
+        """
+        Restore this ROI from a state
+
+        :param curve_plot_item: The plot item that is used for display the curves in the viewer
+        :param view_box:        ViewBox containing the image sequence, used for overlaying the ROIs on top of the image
+        :param state:           ROI state, used for restoring the ROIs. Pass None is not restoring an ROI from a state dict
+        """
         pass
 
 
-class BaseROI(AbstractBaseROI):
-    def __init__(self, curve_plot_item: pg.PlotDataItem, view_box: pg.ViewBox, state: dict = None):
+class BaseROI(_AbstractBaseROI):
+    """
+    A base class that is used by ManualROI and CNMFEROI
+    Inherit from this to make a new ROI class
+    """
+    def __init__(self, curve_plot_item: pg.PlotDataItem, view_box: pg.ViewBox, state: Union[dict, None] = None):
+        """
+        Instantiate common attributes
+
+        :param curve_plot_item: The plot item that is used for display the curves in the viewer
+        :param view_box:        ViewBox containing the image sequence, used for overlaying the ROIs on top of the image
+        :param state:           ROI state, used for restoring the ROIs. Pass None is not restoring an ROI from a state dict
+
+        """
         super().__init__(curve_plot_item, view_box, state)
         if isinstance(curve_plot_item, pg.PlotDataItem):
             self.curve_plot_item = curve_plot_item
@@ -108,12 +195,15 @@ class BaseROI(AbstractBaseROI):
 
         if state is None:
             try:
+                # Set the Tags list from the project configuration
                 roi_defs = get_proj_config().options('ROI_DEFS')
                 self._tags = dict.fromkeys(roi_defs)
             except NoProjectOpen as e:
                 self._tags = {}
+                # If a project isn't open
                 warn('BaseROI subclass instance cannot load ROI_DEFS, no Project open.')
         else:
+            # Restore states
             self._tags = state['tags']
             self.curve_data = state['curve_data']
 
@@ -123,21 +213,22 @@ class BaseROI(AbstractBaseROI):
         self._original_color = None
 
     @property
-    def curve_data(self):
+    def curve_data(self) -> tuple:
         return self.curve_plot_item.getData()
 
     @curve_data.setter
-    def curve_data(self, data: list):
-        """
-        :param data: [x, y], [np.ndarray, np.ndarray]
+    def curve_data(self, data: tuple):
         """
 
+        :rtype: tuple
+        """
         if self.curve_plot_item is None:
             return
         self.curve_plot_item.setData(x=data[0], y=data[1])
 
     @property
-    def zValue(self):
+    def zValue(self) -> int:
+        """ZValue of the curve plot graphics object and roi_graphics_object"""
         return self._zValue
 
     @zValue.setter
@@ -166,6 +257,7 @@ class BaseROI(AbstractBaseROI):
         self.curve_plot_item.setPen(pen)
         if isinstance(self.roi_graphics_object, pg.ScatterPlotItem):
             self.roi_graphics_object.setBrush(pg.mkBrush(color, *args, **kwargs))
+        self._color = color
 
     def set_text(self, text: str):
         text_item = pg.TextItem(text)
@@ -198,6 +290,7 @@ class BaseROI(AbstractBaseROI):
         del roi
 
     def to_state(self):
+        """Must be implemented in subclass"""
         raise NotImplementedError("Must be implemented in subclasses")
 
     @classmethod
@@ -206,9 +299,11 @@ class BaseROI(AbstractBaseROI):
 
 
 class ManualROI(BaseROI):
+    """A class manually drawn ROIs"""
     def __init__(self, curve_plot_item: pg.PlotDataItem,
                  roi_graphics_object: pg.ROI,
-                 view_box: pg.ViewBox, state=None):
+                 view_box: pg.ViewBox,
+                 state: Union[dict, None] = None):
         """
         :type state: dict
         """
@@ -247,6 +342,7 @@ class ManualROI(BaseROI):
 
     @staticmethod
     def get_generic_roi_graphics_object(shape: str, dims: tuple) -> pg.ROI:
+        """Get a generic pg.ROI instance to add to the viewer. Its state can later be set from a saved state or it can be modified by the user"""
         x = dims[0]
         y = dims[1]
 
@@ -270,6 +366,8 @@ class ManualROI(BaseROI):
     @classmethod
     def from_positions(cls, curve_plot_item: pg.PlotDataItem, view_box: pg.ViewBox, positions: list):
         """
+        From a list of positions. Used if importing ImageJ ROIs, can be used for other purposes as well.
+
         :param positions: list of (x, y) tuples; [(x1, y1), (x2, y2), ... (xn, yn)]
         """
         roi_graphics_object = pg.PolyLineROI(positions=positions, closed=True, removable=True)
@@ -277,10 +375,13 @@ class ManualROI(BaseROI):
 
 
 class CNMFROI(BaseROI):
+    """A class for ROIs imported from CNMF(E) output data"""
     def __init__(self, curve_plot_item: pg.PlotDataItem,
                  view_box: pg.ViewBox, cnmf_idx: int = None,
-                 curve_data=None, contour=None, state=None):
+                 curve_data=None, contour=None, state: Union[dict, None] = None):
         """
+        Instantiate attributes.
+
         :type: curve_data:  np.ndarray
         :param curve_data:  1D numpy array of y values
         :type  contour:     np.ndarray
@@ -289,8 +390,8 @@ class CNMFROI(BaseROI):
         """
         super(CNMFROI, self).__init__(curve_plot_item, view_box, state)
 
-        self.roi_xs = np.empty(0)
-        self.roi_ys = np.empty(0)
+        self.roi_xs = np.empty(0)  #: numpy array of the x values of the ROI's spatial coordinates
+        self.roi_ys = np.empty(0)  #: numpy array of the y values of the ROI's spatial coordinates
 
         if state is None:
             self.set_roi_graphics_object(contour)
@@ -301,6 +402,7 @@ class CNMFROI(BaseROI):
             #self.cnmf_idx = cnmf_idx
 
     def set_curve_data(self, y_vals):
+        """Set the curve data"""
         xs = np.arange(len(y_vals))
         self.curve_data = [xs, y_vals]
 
@@ -331,6 +433,7 @@ class CNMFROI(BaseROI):
         self._create_scatter_plot()
 
     def _create_scatter_plot(self):
+        """Create the scatter plot that is used for visualization of the spatial localization"""
         self.roi_graphics_object = pg.ScatterPlotItem(self.roi_xs, self.roi_ys, symbol='s')
 
     def to_state(self) -> dict:
