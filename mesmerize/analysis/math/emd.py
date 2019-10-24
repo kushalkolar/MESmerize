622b6758f8c6ae5882e0921e37cd32c9b4a09e3f
kushalkolar <kushalkolar@gmail.com>
2019-10-24 22:14:22 +0200

    added docstring

--- mesmerize/analysis/math/emd.py
+++ mesmerize/analysis/math/emd.py
@@ -14,6 +14,14 @@ from cv2 import EMD, DIST_L2
 
 
 def emd_1d(u: np.ndarray, v: np.ndarray) -> float:
+    """
+    Wrapper around OpenCV Earth Mover's Distance (EMD) function to calculate EMD between 1D arrays
+
+    :param u: an array of weights
+    :param v: an array of weights
+    :return: Earth Mover's Distance between u & v
+    """
+
     a = np.empty((u.size, 2), dtype=np.float32)
     b = np.empty((v.size, 2), dtype=np.float32)
 
