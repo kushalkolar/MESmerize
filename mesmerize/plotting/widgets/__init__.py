e5312cf6897d598ea0273cd3309d4dee70378de2
kushalkolar <kushalkolar@gmail.com>
2019-11-04 06:29:39 +0100

    cleanup

--- mesmerize/plotting/widgets/__init__.py
+++ mesmerize/plotting/widgets/__init__.py
@@ -7,12 +7,10 @@ from .scatter import ScatterPlotWidget
 
 from .beeswarms import BeeswarmPlotWindow
 from .curve_plot_window import CurvePlotWindow
-from .lda import LDAPlot
 
-from .sosd_fourier import SOSD_Widget
 from .proportions import ProportionsWidget
 
 __all__ = ['DatapointTracerWidget', 'KShapeWidget', 'HeatmapWidget', 'HeatmapSplitterWidget',
            'HeatmapTracerWidget', 'CrossCorrelationWidget', 'ScatterPlotWidget', 'BeeswarmPlotWindow',
-           'CurvePlotWindow', 'LDAPlot', 'SOSD_Widget', 'ProportionsWidget']
+           'CurvePlotWindow', 'ProportionsWidget']
 
