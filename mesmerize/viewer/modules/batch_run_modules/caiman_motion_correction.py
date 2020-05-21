ab701c8d6250359f46263140250250d700a5cac8
Kushal Kolar <kushalkolar@gmail.com>
2020-05-21 20:17:13 +0200

    remove cuda from snap branch

--- mesmerize/viewer/modules/batch_run_modules/caiman_motion_correction.py
+++ mesmerize/viewer/modules/batch_run_modules/caiman_motion_correction.py
@@ -70,11 +70,6 @@ def run(batch_dir: str, UUID: str):
         else:
             gSig_filt = None
 
-        if os.environ['_MESMERIZE_USE_CUDA'] == 'True':
-            USE_CUDA = True
-        else:
-            USE_CUDA = False
-
         min_mov = cm.load(fname[0], subindices=range(200)).min()
 
         mc = MotionCorrect(fname[0], min_mov,
@@ -83,7 +78,7 @@ def run(batch_dir: str, UUID: str):
                            strides=strides, overlaps=overlaps, splits_els=splits_els,
                            upsample_factor_grid=upsample_factor_grid,
                            max_deviation_rigid=max_deviation_rigid,
-                           shifts_opencv=True, nonneg_movie=True, use_cuda=USE_CUDA, gSig_filt=gSig_filt)
+                           shifts_opencv=True, nonneg_movie=True, gSig_filt=gSig_filt)
 
         mc.motion_correct_pwrigid(save_movie=True)
         m_els = cm.load(mc.fname_tot_els)
