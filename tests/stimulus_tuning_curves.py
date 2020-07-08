a1686a7af0c22c1f2cbaf2bfce56b0f9cbc66990
kushalkolar <kushalkolar@gmail.com>
2020-07-09 01:14:24 +0200

    tests

---
+++ tests/stimulus_tuning_curves.py
@@ -0,0 +1,22 @@
+from PyQt5 import QtWidgets
+from mesmerize import Transmission
+from mesmerize.plotting import TuningCurvesWidget
+
+
+def main():
+    t = Transmission.from_hdf5('/work/kushal/pvc7.trn')
+    t.set_proj_path('/share/data/temp/kushal/pvc7_subdataset_mesmerize')
+    t.set_proj_config()
+
+    w = TuningCurvesWidget()
+    w.set_input(t)
+    w.show()
+    return w
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+
+    a = main()
+
+    app.exec()
