a891b1a1eb58464011d0a0dd757e87ae4da3339a
kushalkolar <kushalkolar@gmail.com>
2020-07-09 16:50:54 +0200

    remove detrend dfof node. users should do this in the viewer now

--- mesmerize/pyqtgraphCore/flowchart/library/Biology.py
+++ mesmerize/pyqtgraphCore/flowchart/library/Biology.py
@@ -2,7 +2,6 @@
 # from . import functions
 from .common import *
 from ....analysis.data_types import *
-from caiman.source_extraction.cnmf.utilities import detrend_df_f
 from ....analysis.stimulus_extraction import StimulusExtraction
 from ....plotting import TuningCurvesWidget
 
@@ -81,86 +80,6 @@ class StimTuning(CtrlNode):
         self.t = Transmission.merge(self.transmissions_list)
         self.widget.set_input(self.t)
         return None
-
-
-class DetrendDFoF(CtrlNode):
-    """Uses detrend_df_f from Caiman library."""
-    nodeName = 'DetrendDFoF'
-    uiTemplate = [('quantileMin', 'intSpin', {'min': 1, 'max': 100, 'step': 1, 'value': 20}),
-                  ('frames_window', 'intSpin', {'min': 2, 'max': 5000, 'step': 10, 'value': 100}),
-                  ('auto_quantile', 'check', {'checked': True, 'toolTip': 'determine quantile automatically'}),
-                  ('fast_filter', 'check', {'checked': True, 'tooTip': 'use approximate fast percentile filtering'}),
-                  ('Apply', 'check', {'applyBox': True, 'checked': False})]
-
-    def processData(self, transmission: Transmission):
-        if self.ctrls['Apply'].isChecked() is False:
-            return
-        self.t = transmission.copy()
-        # self.df = t.df
-
-        self.params = {'quantileMin': self.ctrls['quantileMin'].value(),
-                       'frames_window': self.ctrls['frames_window'].value(),
-                       'auto_quantile': self.ctrls['auto_quantile'].isChecked(),
-                       'fast_filter': self.ctrls['fast_filter'].isChecked(),
-                       'output_column': '_DETREND_DF_O_F'
-                       }
-
-        self.t.df.sort_values(by=['SampleID', 'CurvePath'], inplace=True)
-        self.t.df = self.t.df.reset_index(drop=True)
-
-        self._load_data()
-
-        self.t.history_trace.add_operation('all', operation='detrend_df_o_f', parameters=self.params)
-        self.t.last_output = '_DETREND_DF_O_F'
-
-        return self.t
-
-    def _load_data(self):
-        self.current_sample_id = None
-        self.ix = 0
-        dfof_curves = None
-
-        proj_path = self.t.get_proj_path()
-        self.t.df['_DETREND_DF_O_F'] = [np.array([], dtype=np.float64)] * self.t.df.index.size
-        for index, row in self.t.df.iterrows():
-            self.ix += 1
-
-            if row['SampleID'] != self.current_sample_id:
-                self.idx_components = None
-                self.ix = 0
-                dfof_curves = None
-
-                self.current_sample_id = row['SampleID']
-
-                pikPath = os.path.join(proj_path, row['ImgInfoPath'])
-                pik = pickle.load(open(pikPath, 'rb'))
-
-                cnmA = pik['roi_states']['cnmf_output']['cnmA']
-                cnmb = pik['roi_states']['cnmf_output']['cnmb']
-                cnmC = pik['roi_states']['cnmf_output']['cnmC']
-                cnm_f = pik['roi_states']['cnmf_output']['cnm_f']
-                cnmYrA = pik['roi_states']['cnmf_output']['cnmYrA']
-
-                self.idx_components = pik['roi_states']['cnmf_output']['idx_components']
-                try:
-                    dfof_curves = detrend_df_f(cnmA, cnmb, cnmC, cnm_f, YrA=cnmYrA,
-                                               quantileMin=self.params['quantileMin'],
-                                               frames_window=self.params['frames_window'])  # ,
-                    # self.dfof_curves = dfof_curves
-                except:
-                    tb = traceback.format_exc()
-                    raise Exception('The following exception was raised in caiman detrend_df_f method for SampleID: '
-                                    + self.current_sample_id + '\n\n' + tb)
-                # flag_auto=self.ctrls['auto_quantile'].isChecked(),
-                # use_fast=self.ctrls['fast_filter'].isChecked())
-                if self.idx_components[self.ix] != row['ROI_State']['cnmf_idx']:
-                    raise Exception(
-                        'CNMF component index Mismatch Error! Something went very wrong. Check the indices of your '
-                        'CNMF components from SampleID: ' + self.current_sample_id +
-                        '\nIndex in pickle is: ' + str(self.idx_components[self.ix]) +
-                        '\nIndex in dataframe is: ' + str(row['ROI_State']['cnmf_idx']) + '\n at iloc: ' + str(index))
-
-            self.t.df.at[index, '_DETREND_DF_O_F'] = dfof_curves[self.idx_components[self.ix]]
 
 
 class StaticDFoFo(CtrlNode):
