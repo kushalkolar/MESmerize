8990c68f150f30bac99008b4a5724c956d790cce
Blastorios <jordi.zwiggelaar@hotmail.com>
2020-07-22 00:13:25 +0200

    Corrected script

--- scripts/travisScript.sh
+++ scripts/travisScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then 
-    ln -s /usr/local/lib/python36/site-packages/numpy/core/include/numpy numpy && python -m pip3 install -q -r requirements.txt
+    ln -s /usr/local/lib/python36/site-packages/numpy/core/include/numpy numpy && python -m pip install -q -r requirements.txt
 fi
