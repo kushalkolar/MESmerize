cebe21964cf232789a3ffa938ba7fc46c29828fa
kushalkolar <kushalkolar@gmail.com>
2020-08-24 01:39:28 +0200

    nuset module layout works

--- mesmerize/viewer/modules/nuset_segment.py
+++ mesmerize/viewer/modules/nuset_segment.py
@@ -8,9 +8,11 @@ GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 """
 
 import os
+
 # disable tf deprecation warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
+
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 import tifffile
@@ -34,13 +36,14 @@ def get_projection(img: np.ndarray, proj: str = 'std') -> np.ndarray:
 
 def get_preprocess(
         img: np.ndarray,
+        do_preprocess: bool = True,
         do_sigmoid: bool = True,
         sigmoid_cutoff: float = 40.0,
         sigmoid_gain: float = 0.10,
         sigmoid_invert: bool = False,
         do_equalize: bool = True,
         equalize_kernel: int = 8,
-    ) -> np.ndarray:
+) -> np.ndarray:
     """
 
     :param img: input image
@@ -52,6 +55,9 @@ def get_preprocess(
     :param equalize_kernel: Size of equalization kernel
     :return: preprocessed image
     """
+    if not do_preprocess:
+        return normalize_image(img, np.float32)
+
     img = img.astype(np.float32)
 
     if do_sigmoid:
@@ -73,8 +79,39 @@ def get_preprocess(
     return img
 
 
-def get_postprocess(img: np.ndarray) -> np.ndarray:
-    pass
+# from NuSeT processing cleanimage function
+def get_postprocess(
+        img: np.ndarray,
+        do_postprocess: bool = True,
+        abs_obj_threshold: int = -1,
+        rel_obj_threshold: int = 5,
+        obj_connectivity: int = 2,
+        abs_hol_threshold: int = -1,
+        rel_hol_threshold: int = 5,
+        hol_connectivity: int = 2
+    ) -> np.ndarray:
+
+    if not do_postprocess:
+        return img
+
+    image = img.astype(np.bool)
+    im_label = skimage.morphology.label(image, connectivity=1)
+    num_cells = np.max(im_label)
+    mean_area = np.sum(image).astype(np.float32) / num_cells
+
+    if abs_obj_threshold == -1:
+        min_obj_size = mean_area / rel_obj_threshold
+    else:
+        min_obj_size = abs_obj_threshold
+    image = skimage.morphology.remove_small_objects(image, min_size=min_obj_size, connectivity=obj_connectivity)
+
+    if abs_hol_threshold == -1:
+        min_hol_size = mean_area / rel_hol_threshold
+    else:
+        min_hol_size = abs_hol_threshold
+    image = skimage.morphology.remove_small_holes(image, min_size=min_hol_size, connectivity=hol_connectivity)
+
+    return image.astype(np.uint8)
 
 
 # adapted from https://github.com/ver228/tierpsy-tracker/blob/master/tierpsy/analysis/compress/compressVideo.py
@@ -100,6 +137,7 @@ class ModuleGUI(QtWidgets.QWidget):
     def __init__(self, parent, viewer_ref):
         # self.vi = ViewerUtils(viewer_ref)
         QtWidgets.QWidget.__init__(self, parent)
+        self.resize(1000, 900)
 
         self.vlayout = QtWidgets.QVBoxLayout(self)
 
@@ -108,39 +146,193 @@ class ModuleGUI(QtWidgets.QWidget):
 
         self.left_layout = QtWidgets.QVBoxLayout(self)
 
-        img_ignore = {'img': {'ignore': True}}
-
-        self.preprocess_controls = Function(get_preprocess, arg_opts=img_ignore, parent=self.splitter)
-
         self.left_widget = QtWidgets.QWidget(self.splitter)
         self.left_widget.setLayout(self.left_layout)
 
+        self.preprocess_controls = \
+            Function(
+                get_preprocess,
+                arg_opts={'img': {'ignore': True}},
+                parent=self.left_widget
+            )
+        self.preprocess_controls.button_set.hide()
+        self.preprocess_controls.sig_changed.connect(self.update_preprocess)
+        self.preprocess_controls.set_title("Pre-process Settings")
+
+        hlayout_projection_radios = QtWidgets.QHBoxLayout(self.left_widget)
+        self.radio_std = QtWidgets.QRadioButton(self.left_widget)
+        self.radio_std.setText("Std. Deviation")
+        self.radio_max = QtWidgets.QRadioButton(self.left_widget)
+        self.radio_max.setText("Max")
+        self.radio_mean = QtWidgets.QRadioButton(self.left_widget)
+        self.radio_mean.setText("Mean")
+
+        for w in [self.radio_std, self.radio_max, self.radio_mean]:
+            hlayout_projection_radios.addWidget(w)
+
+        label_projections = QtWidgets.QLabel(self.left_widget)
+        label_projections.setText("Choose Input Projection")
+        label_projections.setStyleSheet("font-weight: bold")
+
+        self.left_layout.addWidget(label_projections)
+        self.left_layout.addLayout(hlayout_projection_radios)
+
+        # projection image
         self.glw_raw = GraphicsLayoutWidget(self.left_widget)
-        self.img_item_raw = ImageItem()
+        self.imgitem_raw = ImageItem()
         self.viewbox_raw = self.glw_raw.addViewBox()
-        self.viewbox_raw.addItem(self.img_item_raw)
+        self.viewbox_raw.setAspectLocked(True)
+        self.viewbox_raw.addItem(self.imgitem_raw)
 
         self.left_layout.addWidget(self.glw_raw)
-
-        # self.left_layout.addWidget(self.img_item_raw)
         self.left_layout.addWidget(self.preprocess_controls.widget)
 
-        # self.img_preprocessed = ImageItem()
-        # self.left_layout.addWidget(self.img_preprocessed)
+        # preprocess image
+        self.glw_preprocess = GraphicsLayoutWidget(self.left_widget)
+        self.imgitem_preprocess = ImageItem()
+        self.viewbox_preprocess = self.glw_preprocess.addViewBox()
+        self.viewbox_preprocess.setAspectLocked(True)
+        self.viewbox_preprocess.addItem(self.imgitem_preprocess)
+
+        self.left_layout.addWidget(self.glw_preprocess)
 
         self.splitter.addWidget(self.left_widget)
+
+        # segmentation stuff
+        self.nuset_model = Nuset()
 
         self.right_layout = QtWidgets.QVBoxLayout(self)
 
         self.right_widget = QtWidgets.QWidget(self.splitter)
         self.right_widget.setLayout(self.right_layout)
 
-        self.glw_raw2 = GraphicsLayoutWidget(self.right_widget)
-        self.img_item_raw2 = ImageItem()
-        self.viewbox_raw2 = self.glw_raw2.addViewBox()
-        self.viewbox_raw2.addItem(self.img_item_raw2)
+        hlayout_nuset = QtWidgets.QHBoxLayout(self.right_widget)
 
-        self.right_layout.addWidget(self.glw_raw2)
+        arg_opts_nuset = \
+            {
+                'image': {'ignore': True},
+
+                'min_score': {
+                    'minmax': (0.0, 1.0),
+                    'step': 0.05
+                },
+
+                'nms_threshold': {
+                    'minmax': (0.0, 1.0),
+                    'step': 0.05
+                },
+
+                'rescale_ratio': {
+                    'minmax': (0.1, 10.0),
+                    'step': 0.5
+                }
+
+            }
+
+        self.nuset_controls = \
+            Function(
+                self.nuset_model.predict,
+                arg_opts=arg_opts_nuset,
+                parent=self.right_widget
+            )
+
+        self.nuset_controls.button_set.hide()
+        self.nuset_controls.sig_changed.connect(self.update_segmentation)
+        self.nuset_controls.set_title("NuSeT Parameters")
+        hlayout_nuset.addWidget(self.nuset_controls.widget)
+
+        arg_opts_postprocess = \
+            {
+                'img': {'ignore': True},
+
+                'abs_obj_threshold': {
+                    'minmax': (-1, 100),
+                    'step': 2,
+                },
+
+                'rel_obj_threshold': {
+                    'minmax': (0, 100),
+                    'step': 2
+                },
+
+                'obj_connectivity': {
+                    'minmax': (0, 10),
+                    'step': 1
+                },
+
+                'abs_hol_threshold': {
+                    'minmax': (-1, 100),
+                    'step': 2,
+                },
+
+                'rel_hol_threshold': {
+                    'minmax': (0, 100),
+                    'step': 2
+                },
+
+                'hol_connectivity': {
+                    'minmax': (0, 100),
+                    'step': 1
+                }
+            }
+
+        self.postprocess_controls = \
+            Function(
+                get_postprocess,
+                arg_opts=arg_opts_postprocess,
+                parent=self.right_widget
+            )
+        self.postprocess_controls.button_set.hide()
+        self.postprocess_controls.sig_changed.connect(self.update_postprocess)
+        self.postprocess_controls.set_title("Post-process Parameters")
+
+        hlayout_nuset.addWidget(self.postprocess_controls.widget)
+        self.right_layout.addLayout(hlayout_nuset)
+
+        self.glw_segmented = GraphicsLayoutWidget(self.right_widget)
+        self.imgitem_segmented = ImageItem()
+        self.viewbox_segmented = self.glw_segmented.addViewBox()
+        self.viewbox_segmented.addItem(self.imgitem_segmented)
+
+        self.right_layout.addWidget(self.glw_segmented)
         self.splitter.addWidget(self.right_widget)
 
+        self._image_projection: np.ndarray = np.empty(0)
+        self._image_preprocess: np.ndarray = np.empty(0)
+        self._image_segmented: np.ndarray = np.empty(0)
 
+    def update_projection(self):
+        pass
+
+    def update_preprocess(self, params):
+        pass
+
+    def update_segmentation(self, params):
+        pass
+
+    def update_postprocess(self, params):
+        print(params)
+
+    @property
+    def image_projection(self) -> np.ndarray:
+        return self._image_projection
+
+    @image_projection.setter
+    def image_projection(self, img: np.ndarray):
+        pass
+
+    @property
+    def image_preprocess(self) -> np.ndarray:
+        return self._image_preprocess
+
+    @image_preprocess.setter
+    def image_preprocess(self, img: np.ndarray):
+        pass
+
+    @property
+    def image_segmented(self) -> np.ndarray:
+        return self._image_segmented
+
+    @image_segmented.setter
+    def image_segmented(self, img: np.ndarray):
+        pass
--- tests/nuset_test.py
+++ tests/nuset_test.py
@@ -1,9 +1,16 @@
 from mesmerize.viewer.modules.nuset_segment import *
+import tifffile
+import numpy as np
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
 
+    img = tifffile.imread('/home/kushal/Sars_stuff/zfish_stds/5.tiff')
+
     w = ModuleGUI(None, None)
     w.show()
+
+    w.imgitem_raw.setImage(img)
 
     app.exec_()
