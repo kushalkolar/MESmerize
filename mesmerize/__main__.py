4210dd3de879f5093cf38b51ce574bd9df6a8339
kushalkolar <kushalkolar@gmail.com>
2020-09-12 12:43:29 +0200

    some organization in __main__, added show-graph and open-plot args

--- mesmerize/__main__.py
+++ mesmerize/__main__.py
@@ -20,6 +20,9 @@ from mesmerize.common.window_manager import WindowManager
 from mesmerize.project_manager import ProjectManager
 from mesmerize.common.welcome_window import MainWindow
 from mesmerize.scripts import *
+from mesmerize import Transmission
+from mesmerize.plotting import open_plot_file
+import os
 
 
 class App(QApplication):
@@ -31,7 +34,8 @@ def start_welcome_window():
     app = App([])
     app.window_manager.welcome_window = MainWindow()
     app.window_manager.welcome_window.show()
-    app.exec_()
+
+    return app
 
 
 def start_batch_manager(batch_path: str, item_uuid: str):
@@ -39,19 +43,45 @@ def start_batch_manager(batch_path: str, item_uuid: str):
     app.window_manager.welcome_window = MainWindow()
     app.window_manager.welcome_window.show()
     bm = app.window_manager.get_batch_manager(run_batch=[batch_path, item_uuid])
-    app.exec_()
+    return (app, bm)
 
 
 def main():
     if not len(sys.argv) > 1:
-        start_welcome_window()
+        app = start_welcome_window()
+        app.exec_()
 
     elif sys.argv[1] == 'run':
         if sys.argv[2] == 'batch':
-            start_batch_manager(sys.argv[3], sys.argv[4])
+            app, bm = start_batch_manager(sys.argv[3], sys.argv[4])
+            app.exec_()
 
     elif sys.argv[1] == 'lighten':
         create_lite_project.main(*sys.argv[2:])
+
+    elif sys.argv[1] == 'show-graph':
+        path = sys.argv[2]
+        if not os.path.isfile(path):
+            raise FileNotFoundError("File does not exist")
+
+        t = Transmission.from_hdf5(path)
+        dbs = t.history_trace.data_blocks
+        for db in dbs:
+            t.history_trace.draw_graph(data_block_id=db, view=True)
+
+    elif sys.argv[1] == 'open-plot':
+        path = sys.argv[2]
+        proj_path = os.path.dirname(
+            os.path.dirname(path)
+        )
+        app = start_welcome_window()
+
+        app.window_manager.welcome_window.hide()
+        app.project_manager.set(project_root_dir=proj_path)
+        plot = open_plot_file(path)
+        plot.show()
+
+        app.exec_()
 
     else:
         raise ValueError('Invalid argument')
