1214e6cb32c9b4ea297ec3f634a54b7fcb069a9f
kushalkolar <kushalkolar@gmail.com>
2020-10-13 08:35:34 +0200

    kshape grid finished, need to test

--- mesmerize/plotting/widgets/kshape/kshape_process.py
+++ mesmerize/plotting/widgets/kshape/kshape_process.py
@@ -13,10 +13,14 @@ import sys
 import numpy as np
 import pickle
 import os
-from ....common.configuration import HAS_TSLEARN
+from ....common.configuration import HAS_TSLEARN, get_sys_config
 if HAS_TSLEARN:
     from tslearn.clustering import KShape
-
+from joblib import Parallel, delayed
+from typing import *
+from scipy import signal
+from itertools import product
+import json
 
 def run(data_path: str, params_path: str):
     X = np.load(data_path)
@@ -34,6 +38,13 @@ def run(data_path: str, params_path: str):
     n_train = int((params['kwargs'].pop('train_percent') / 100) * X.shape[0])
     train = X[np.random.choice(X.shape[0], size=n_train, replace=False)]
 
+    if params['do_single']:
+        run_single(X, train, params, workdir, out)
+    else:
+        run_grid(X, train, params, workdir, out)
+
+
+def run_single(X, train, params, workdir, out):
     kwargs = params['kwargs']
     ks = KShape(**kwargs)
 
@@ -50,6 +61,77 @@ def run(data_path: str, params_path: str):
 
     train_path = os.path.join(workdir, 'train.npy')
     np.save(train_path, train)
+
+    with open(out, 'w') as f:
+        f.write('1')
+
+    print('* Done! *')
+
+
+# np.array_split(a, n_partitions, axis=0)
+
+def sort_peak_widths(a: np.ndarray):
+    widths = [signal.peak_widths(p, [np.argmax(p)])[0][0] for p in a]
+    sort_ixs = np.argsort(widths)
+
+    return a[sort_ixs]
+
+
+def sort_amplitidue(a: np.ndarray):
+    ampls = [s.argmax() for s in a]
+    sort_ixs = np.argsort(ampls)
+
+    return a[sort_ixs]
+
+
+def kshape_grid_iter(X_partitioned: List[np.array], kshape_kwargs: dict) -> KShape:
+    seed_ixs = [np.random.randint(0, X.shape[0] - 1) for X in X_partitioned]
+    centroid_seeds = np.array([X_partitioned[i][seed] for i, seed in enumerate(seed_ixs)])
+    init = np.swapaxes(np.array([centroid_seeds]).T, 0, 1)
+
+    kshape = KShape(
+        n_clusters=len(seed_ixs),
+        init=init,
+        verbose=True,
+        random_state=None
+        **kshape_kwargs
+    )
+
+    X = np.vstack(X_partitioned)
+
+    kshape.fit(X)
+
+    # until the tslearn hyper-param json issue is released in the latest pypi version
+    kshape.init = kshape.init.tolist()
+
+    return kshape
+
+
+def run_grid(X, train, params, workdir, out):
+    sortby = params.pop('sortby')
+    if sortby == 'width':
+        X_sorted = sort_peak_widths(X)
+    elif sortby == 'amplitude':
+        X_sorted = sort_amplitidue(X)
+
+    p_range = params.pop('npartitions_range')
+    ncombinations = params.pop('ncombinations')
+
+    n_jobs = int(get_sys_config()['_MESMERIZE_N_THREADS'] / 2)
+    kg = Parallel(n_jobs=n_jobs, verbose=5)(
+        delayed(kshape_grid_iter)(
+            np.array_split(X_sorted, nparts, axis=0),
+            ) for nparts in range(*p_range) \
+                for i in range(ncombinations)
+    )
+
+    # kga = np.array(kg, dtype=object).reshape(ncombinations, len(range(*p_range)))
+
+    kga = [k._to_dict(output='json') for k in kg]
+
+    json.dump(kga, open(os.path.join(workdir, 'kga.json'), 'w'))
+
+    # pickle.dump(kga, open(os.path.join(workdir, 'kga.pickle'), 'wb'))
 
     with open(out, 'w') as f:
         f.write('1')
