775704cb40f77d6b56a639870012493c96a5fd47
kushalkolar <kushalkolar@gmail.com>
2020-11-27 01:12:52 +0100

    fix cnmfe bugs, uses the CNMFParams object instead of feeding kwargs directly to the CNMF object

--- mesmerize/viewer/modules/cnmfe.py
+++ mesmerize/viewer/modules/cnmfe.py
@@ -69,17 +69,21 @@ class ModuleGUI(QtWidgets.QDockWidget):
                 'gSig': gSig
             }
 
+        kval = self.ui.spinBoxK.value()
+        if kval == 0:
+            kval = None
+
         # CNMFE kwargs
         cnmfe_kwargs = \
             {
                 'gSig': (gSig, gSig),
-                'gSiz': (3 * gSig + 1, 3 * gSig + 1),
+                'gSiz': (4 * gSig + 1, 4 * gSig + 1),
                 'border_pix': bord_px,
                 'rf': self.ui.spinBoxRf.value(),
                 'stride': self.ui.spinBoxOverlap.value(),
                 'gnb': self.ui.spinBoxGnb.value(),
                 'nb_patch': self.ui.spinBoxNb_patch.value(),
-                'k': self.ui.spinBoxK.value(),
+                'K': kval,
                 'min_corr': self.ui.doubleSpinBoxMinCorr.value(),
                 'min_pnr': self.ui.spinBoxMinPNR.value(),
                 'deconv_flag': deconv_flag,
@@ -117,7 +121,7 @@ class ModuleGUI(QtWidgets.QDockWidget):
         if self.ui.groupBox_eval_kwargs.isChecked():
             try:
                 _kwargs = self.ui.plainTextEdit_eval_kwargs.toPlainText()
-                eval_kwargs_add = eval(f"dict{_kwargs}")
+                eval_kwargs_add = eval(f"dict({_kwargs})")
                 eval_kwargs.update(eval_kwargs_add)
             except:
                 raise ValueError("Evaluation kwargs not formatted properly.")
