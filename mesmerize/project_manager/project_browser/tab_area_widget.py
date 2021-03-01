35dafda0c8e65e61348dc29b8178ee77348298e2
kushalkolar <kushalkolar@gmail.com>
2021-03-01 10:59:26 +0100

    ok not possible without large message window, handled at higher level, nvm

--- mesmerize/project_manager/project_browser/tab_area_widget.py
+++ mesmerize/project_manager/project_browser/tab_area_widget.py
@@ -280,7 +280,6 @@ class TabAreaWidget(QtWidgets.QWidget):
             try:
                 self._populate_column(column, self.dataframe[column.column_name])
             except Exception:
-                print(traceback.format_exc())
                 raise ValueError(f"Cannot open project due to an issue with the following column: `{column.column_name}`\n"
                                  f"See the terminal for a full traceback")
 
