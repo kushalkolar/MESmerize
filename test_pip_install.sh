bc881755a9aba15ed906705cb0cc248c7d399297
JCZwiggelaar <53938178+Blastorios@users.noreply.github.com>
2021-11-04 00:08:45 +0100

    Delete test_pip_install.sh

--- test_pip_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf /share/python-virtual-environments/mesmerize-pip-testing &&
-python3 -m venv /share/python-virtual-environments/mesmerize-pip-testing &&
-source /share/python-virtual-environments/mesmerize-pip-testing &&
-pip install --upgrade pip setuptools
-pip install Cython numpy
-pip install mesmerize --extra-index-url=https://test.pypi.org/simple/
-which mesmerize
-
